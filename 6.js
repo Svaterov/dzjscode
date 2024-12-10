afd4839bc810d452b54d8a517d040733135d65f3
Svaterov <Ghostilin351@gmail.com>
2024-12-10 21:25:29 +0300

    gggg

---
+++ 6.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+let dallar = prompt("введите количество долларов: ");
+const skolko = 0.95
+let euro = dollar * 0.95;
+console.log("количество евро ${result}");
