61fca62c951f19b793f45032c9b603d310b22288
Svaterov <Ghostilin351@gmail.com>
2024-12-10 21:08:32 +0300

    fffffffff

---
+++ 1.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+let name = prompt('введитесовй год рождения:');
+const year = 2024;
+let result = year - brith;
+console.log("Вам ${result} лет")
---
+++ 4.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+let storona = prompt("введите радиус окружности: ");
+let result = match.PI * match.pow(radius , 2);
+console.log("площадь окружности: ${result}");
